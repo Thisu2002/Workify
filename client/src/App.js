9049b88ad7e93fbeef1ec43bbc63af2eacb0698b
Thisu2002 <thisumevanya@gmail.com>
2025-06-13 23:17:45 +0530

    Initial commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [msg, setMsg] = useState('');
+
+  useEffect(() => {
+    fetch('http://localhost:5000/api/test')
+      .then(response => response.json())
+      .then(data => setMsg(data.message))
+      .catch(error => console.error('Error fetching message:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Workify</h1>
+      <p>{msg || 'Loading...'}</p>
     </div>
   );
 }
