e85ecbcd99ff4892922c6b3a2601f8f00f93933f
Sandaruwani11 <gaweshika11@gmail.com>
2025-07-17 11:57:15 +0530

    user management

--- client/src/components/Admin/AdminContent.js
+++ client/src/components/Admin/AdminContent.js
@@ -2,6 +2,7 @@ import { useLocation } from "react-router-dom";
 import Overview from "./Overview";
 import Analytics from "./Analytics";
 import { Box } from "@mui/material";
+import Users from "./users";
 
 const AdminContent = ({showJobForm, setShowJobForm}) => {
   const location = useLocation();
@@ -11,6 +12,7 @@ const AdminContent = ({showJobForm, setShowJobForm}) => {
       {location.pathname === "/admin" && <Overview />}
       {location.pathname === "/admin/overview" && <Overview />}
       {location.pathname === "/admin/analytics" && <Analytics />}
+      {location.pathname === "/admin/users" && <Users />}
       {/* {location.pathname.startsWith("/pet-owners/view-owner/") && (
             <ViewOwner />
           )} */}
