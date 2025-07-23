fd675511f22b84123523bcad4a6109fd6963214c
Kasunshya Pabodi Jayawardana <kasunshyapabodi2002@gmail.com>
2025-07-23 21:10:39 +0530

    Final cleanup before merge to dev

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ import AdminRoot from "./pages/AdminRoot";
 import BusinessManagerRoot from "./pages/BusinessManagerRoot";
 
 import JobPosts from "./components/Recruiter/JobPosts";
-
+import CandidateList from "./components/LeadPanelist/CandidateList";
 
 
 const App = () => {
@@ -44,6 +44,8 @@ const App = () => {
         <Route path="/lead-panelist/interviews" element={<LeadPanelistRoot />} />
         <Route path="/lead-panelist/evaluations" element={<LeadPanelistRoot />} />
         <Route path="/lead-panelist/feedback" element={<LeadPanelistRoot />} />
+        <Route path="/lead-panelist/assignments" element={<LeadPanelistRoot />} />
+        <Route path="/lead-panelist/candidates" element={<CandidateList />} />
 
        
         <Route path="/recruiter" element={<RecruiterRoot />} />
